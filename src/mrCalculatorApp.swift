acfe0081d3b65c8629378b16f80e2ae85acc5d76
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-08-25 21:38:24 +0200

    Create mrCalculatorApp.swift

---
+++ src/mrCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  mrCalculatorApp.swift
+//  mrCalculator
+//
+//  Created by mrtuxa on 25.08.22.
+//
+
+import SwiftUI
+
+@main
+struct mrCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
